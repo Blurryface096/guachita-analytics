745924ad70af4fe96f261cdd2428b5bfce4cbab6
Alfredo Chuquihuaranga <20140329@aloe.ulima.edu.pe>
2018-02-20 20:50:39 -0500

    agregado heroku a allowed host

--- proy_analiticas/settings.py
+++ proy_analiticas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7$20gudmklhh^i6i&1a_vhvzjnl+f7#^n*dz1z(!##mb(j9f7e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
