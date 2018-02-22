b681bd20c21db5c4559b882622f9de60b980ebd2
Alfredo Chuquihuaranga <20140329@aloe.ulima.edu.pe>
2018-02-22 00:25:36 -0500

    orderby agregado

--- apps/analiticas/views.py
+++ apps/analiticas/views.py
@@ -46,7 +46,7 @@ def getVistasDiarias(request):
     db = client['guachita-analytics']
     pipeline = [
         {"$group" : {"_id":"$Date", "total": {"$sum":1}}},
-        {"$sort": SON([("total", -1), ("_id", -1)])}
+        {"$sort": SON([("_id", 1), ("total", 1)])}
     ]
 
     eventos = db.eventos.aggregate(pipeline)
